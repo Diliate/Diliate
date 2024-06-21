1f40496e10433ac70bb6b61aca39577be05508cc
Danish <danishabdullah276@gmail.com>
2024-06-21 15:24:48 +0530

    s325p

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx', // Set the loader to handle JSX syntax
+    include: /\.(js|jsx)$/, // Apply the loader to both .js and .jsx files
+    exclude: /node_modules/ // Exclude node_modules from being processed by the loader
+  }
 })
+  
